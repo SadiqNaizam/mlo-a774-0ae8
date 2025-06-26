4838a867ebbaabf7d0137f464e8f8daadd0342f2
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-26 13:56:01 +0530

    Updated code files via regeneration: utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
-import { clsx, type ClassValue } from "clsx"
+```ts
+import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+```
